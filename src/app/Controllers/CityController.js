433903db029ef5228812ede8e938022bd72209f3
luisclaudiorp <luis.ramalho@sou.ucpel.edu.br>
2021-09-25 20:09:01 -0300

    all city routes were made

--- src/app/Controllers/CityController.js
+++ src/app/Controllers/CityController.js
@@ -5,15 +5,10 @@ class CityController {
   async showAll (req, res) {
     try {
       const cities = await City.find()
-      if (cities.length === 0) {
-        return res.status(400).json({
-          message: 'No city found'
-        })
-      }
       return res.status(200).send({ cities })
     } catch (error) {
       return res.status(400).json({
-        message: 'No city found'
+        message: 'No cities found'
       })
     }
   }
@@ -37,39 +32,53 @@ class CityController {
     })
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({
-        error: true,
-        message: 'Error invalid data'
+        message: 'Invalid data'
       })
     }
     const cityExist = await City.findOne({ name: req.body.name })
     if (cityExist) {
       return res.status(400).json({
-        error: true,
-        message: 'Error City already registered'
+        message: 'City already registered'
       })
     }
-    const { name, state } = req.body
-    const data = { name, state }
-    await City.create(data, (err) => {
-      if (err) {
-        return res.status(400).json({
-          error: true,
-          message: 'Error entering city'
-        })
-      }
+    try {
+      const { name, state } = req.body
+      const data = { name, state }
+      await City.create(data)
       return res.status(201).json({
-        error: false,
         message: 'city ​​successfully registered'
       })
-    })
+    } catch (error) {
+      return res.status(400).json({
+        message: 'Error entering city'
+      })
+    }
   }
 
   async update (req, res) {
-    res.send({ city: req.params.id })
+    try {
+      const { name, state } = req.body
+      const city = await City.findByIdAndUpdate(req.params.id, {
+        name, state
+      }, { new: true })
+      return res.status(200).send({ city })
+    } catch (error) {
+      return res.status(400).json({
+        message: 'No updating city'
+      })
+    }
   }
 
   async destroy (req, res) {
-    res.send({ city: req.params.id })
+    try {
+      await City.findByIdAndRemove(req.params.id)
+
+      return res.status(200).send()
+    } catch (error) {
+      return res.status(400).json({
+        message: 'No deleting city'
+      })
+    }
   }
 }
 module.exports = new CityController()
